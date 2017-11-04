7cd6bfcd098106af27c1634189e8e241cbd0d6cb
zhaohua <zhaohua@oaserver.dw.gdbbk.com>
2017-11-04 17:40:50 +0800

    Initial commit

---
+++ app/src/main/java/com/vince/volley_notes/VolleyRequstActivity.java
@@ -0,0 +1,17 @@
+package com.vince.volley_notes;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/**
+ * 用Volley模拟请求类
+ */
+public class VolleyRequstActivity extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_volley_requst);
+    }
+}
